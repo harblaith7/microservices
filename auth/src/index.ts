0175bcbc5758915dd342c1be0b7fe253788854b7
Laith Harb <35265876+harblaith7@users.noreply.github.com>
2021-03-14 10:22:04 -0400

    added signup route and ensure consistent error structure

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,22 @@
-import express from "express";
+import express from 'express';
+import { json } from 'body-parser'
+import 'express-async-errors'
 
+import { signupRouter } from "./routes/signup"
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+app.use(json());
 
-app.get("/", (req, res) => {
-    res.send("Working")
+app.use(signupRouter)
+
+app.all("*", () => {
+    throw new NotFoundError()
 })
 
+app.use(errorHandler)
+
 app.listen(3000, () => {
-    console.log("Now listen on port 3000")
+    console.log('Listening on port 3000!!!!')
 })
