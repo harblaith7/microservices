9c9d9ee6b567eb510e485d8d9132f47cded6d535
Laith Harb <35265876+harblaith7@users.noreply.github.com>
2021-03-14 21:40:39 -0400

    added cookiesession to auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,13 +2,21 @@ import express from 'express';
 import { json } from 'body-parser'
 import 'express-async-errors'
 import mongoose from "mongoose"
+import cookieSession from "cookie-session"
 
 import { signupRouter } from "./routes/signup"
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+app.set('trust proxy', true)
 app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true
+    })
+)
 
 app.use(signupRouter)
 
